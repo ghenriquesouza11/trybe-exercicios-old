f6045dcb9f11d4615a068bad44b737540ae8b446
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-25 19:14:40 -0300

    Soma todos os valores do array.

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
@@ -1,4 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for(index = 0; index < numbers.length; index ++) {
+//     console.log(numbers[index]); 
+// }
+var soma = 0;
 for(index = 0; index < numbers.length; index ++) {
-    console.log(numbers[index]); 
-}
+    soma = soma + numbers[index];
+    }
+console.log(soma);
