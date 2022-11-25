11598c2c591b54cfb38a3e480120cfe205ad8b6a
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-25 19:22:35 -0300

    Calcula a média aritimética entre os valores do array

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
@@ -1,9 +1,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Percorre todo o array numbers e imprime cada um dos valores.
 // for(index = 0; index < numbers.length; index ++) {
-//     console.log(numbers[index]); 
+//     console.log(numbers[index]);
 // }
+
+// Soma todos os valores do array.
+// var soma = 0;
+// for(index = 0; index < numbers.length; index ++) {
+//     soma = soma + numbers[index];
+//     }
+// console.log(soma);
+
 var soma = 0;
-for(index = 0; index < numbers.length; index ++) {
-    soma = soma + numbers[index];
-    }
-console.log(soma);
+
+for (index = 0; index < numbers.length; index++) {
+  soma = soma + numbers[index];
+}
+var media = soma / numbers.length;
+console.log(media);
