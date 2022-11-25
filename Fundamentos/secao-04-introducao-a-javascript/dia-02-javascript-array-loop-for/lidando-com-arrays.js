61b8a7a1a9f3331aae3eec9bb7b4f1d8d5a016f1
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-25 19:35:31 -0300

    Imprime o maior número do array

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/lidando-com-arrays.js
@@ -20,13 +20,23 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // var media = soma / numbers.length;
 // console.log(media);
 
-var soma = 0;
-for (index = 0; index < numbers.length; index++) {
-  soma = soma + numbers[index];
-}
-var media = soma / numbers.length;
-if (media > 20) {
-  console.log("valor maior que 20");
-} else if (media < 20) {
-  console.log("valor menor que 20");
-}
+// caso o valor final seja maior que 20, imprime a mensagem: “valor maior que 20”. Caso não seja, imprime a mensagem: “valor menor ou igual a 20”
+// var soma = 0;
+// for (index = 0; index < numbers.length; index++) {
+//   soma = soma + numbers[index];
+// }
+// var media = soma / numbers.length;
+// if (media > 20) {
+//   console.log("valor maior que 20");
+// } else if (media < 20) {
+//   console.log("valor menor que 20");
+// }
+
+// Imprime o maior número do array
+// let maiorNumero = 0;
+// for (index = 0; index < numbers.length; index++) {
+//   if (maiorNumero < numbers[index]) {
+//     maiorNumero = numbers[index];
+//   }
+// }
+// console.log(maiorNumero);
