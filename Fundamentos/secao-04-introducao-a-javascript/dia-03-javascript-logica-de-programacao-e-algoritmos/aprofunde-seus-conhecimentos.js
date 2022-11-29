a4c7088a175617c8dfec0cb816cf427a31898972
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-29 19:55:38 -0300

    Cria um algoritmo para inverter uma palavra.

--- Fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/aprofunde-seus-conhecimentos.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/aprofunde-seus-conhecimentos.js
@@ -1,7 +1,15 @@
-let number = 7;
-let fatorial = number;
-for (index = 1; index < number; index++) {
-  let parametro = number - index;
-  fatorial = fatorial * parametro;
+// let number = 10;
+// let fatorial = number;
+// for (index = 1; index < number; index++) {
+//   let parametro = number - index;
+//   fatorial = fatorial * parametro;
+// }
+// console.log(fatorial);
+
+let word = "tryber";
+let palavraInvertida = [];
+let teste = word.length;
+for (index = word.length - 1; index >= 0; index--) {
+  palavraInvertida.push(word[index]);
 }
-console.log(fatorial);
+console.log(palavraInvertida);
