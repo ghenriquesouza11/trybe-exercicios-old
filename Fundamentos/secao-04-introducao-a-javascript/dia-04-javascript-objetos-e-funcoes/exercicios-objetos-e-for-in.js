d9ef2633a6b28c2f02b375ab0c8bc29c13b4b6da
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-30 16:29:19 -0300

    Cria um segundo objeto e imprime lado a lado os valores de ambos os objetos.

--- Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -13,6 +13,16 @@ let info = {
 //   console.log(keyName);
 // }
 
+// for (index = 0; index < Object.keys(info).length; index++) {
+//   console.log(Object.values(info)[index]);
+// }
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
 for (index = 0; index < Object.keys(info).length; index++) {
-  console.log(Object.values(info)[index]);
+  console.log(Object.values(info)[index], " e ", Object.values(info2)[index]);
 }
