8454dda716e8b787ff3fef965d08f9cd51fe504e
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-30 18:46:58 -0300

    Cria uma função que retorna o índice do maior valor de um array.

--- Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/funcoes.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/funcoes.js
@@ -1,12 +1,28 @@
-function palindromo(palavra) {
-  let palavraInvertida = [];
-  for (index = palavra.length - 1; index >= 0; index--) {
-    palavraInvertida.push(palavra[index]);
+// function palindromo(palavra) {
+//   let palavraInvertida = [];
+//   for (index = palavra.length - 1; index >= 0; index--) {
+//     palavraInvertida.push(palavra[index]);
+//   }
+//   if (palavraInvertida.join("") == palavra) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+function maiorIndice(array) {
+  let maiorValor = 0;
+  for (index = 0; index < array.length; index++) {
+    if (array[index] >= maiorValor) {
+      maiorValor = array[index];
+    }
   }
-  if (palavraInvertida.join("") == palavra) {
-    console.log(true);
-  } else {
-    console.log(false);
+  let maiorValorIndice = 0;
+  for (index = 0; index < array.length; index++) {
+    if (maiorValor == array[index]) {
+      maiorValorIndice = index;
+    }
   }
+  console.log(maiorValorIndice);
 }
-palindromo("arara");
+maiorIndice([2, 3, 6, 7, 10, 1]);
