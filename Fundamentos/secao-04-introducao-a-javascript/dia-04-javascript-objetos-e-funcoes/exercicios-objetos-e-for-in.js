69b4c4e8958898d96fa566e0afa8933b53176000
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-11-30 15:45:53 -0300

    Usa um loop for...in para imprimir o nome de todas as keys do objeto 'info'.

--- Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -6,3 +6,7 @@ let info = {
 console.log("Bem-vinda,", info.personagem, "");
 info["recorrente"] = "Sim";
 console.log(info.recorrente);
+for (index = 0; index < Object.keys(info).length; index++) {
+  let keyName = Object.keys(info)[index];
+  console.log(keyName);
+}
