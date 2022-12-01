7cbc61106d0786b4cbccf789292579d43516a4b3
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-01 15:18:08 -0300

    Corrige o erro relacionado ao escopo da variável do objeto.

--- Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
@@ -1,7 +1,19 @@
-function imprimeIdade() {
-  for (let idade = 30; idade <= 40; idade += 1) {
-    console.log("Idade dentro do for:", idade);
-  }
-  //   console.log("Idade fora do for:", idade); // retire essa linha ao implementar o tipo de variável correta.
-}
-imprimeIdade();
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log("Idade dentro do for:", idade);
+//   }
+//   //   console.log("Idade fora do for:", idade); // retire essa linha ao implementar o tipo de variável correta.
+// }
+// imprimeIdade();
+
+// Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+let pessoa = {
+  nome: "Henri",
+  idade: 20,
+};
+pessoa = {
+  nome: "Luna",
+  idade: 19,
+}; // Altere essa estrutura para corrigir o erro.
+console.log("Nome:", pessoa.nome);
+console.log("Idade:", pessoa.idade);
