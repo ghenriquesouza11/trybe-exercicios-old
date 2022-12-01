0bbbc196926643eff325944d0b44533552ee89eb
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-01 16:43:31 -0300

    Muda a sintaxe da função para arrow function

--- Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
@@ -22,13 +22,18 @@
 // favoriteFood = "Hamburguer";
 // console.log(favoriteFood);
 
-const name = "Adriana";
-const lastName = "Soares";
-console.log(`Olá, ${name} ${lastName}!`);
-function soma(a, b) {
-  let resultado = a + b;
-  return resultado;
-}
-let a = 3;
-let b = 5;
-console.log(`O resultado da soma de  ${a} + ${b} é: ${soma(a, b)}`);
+// const name = "Adriana";
+// const lastName = "Soares";
+// console.log(`Olá, ${name} ${lastName}!`);
+// function soma(a, b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de  ${a} + ${b} é: ${soma(a, b)}`);
+
+numeroAleatorio = () => {
+  return Math.random();
+};
+console.log(numeroAleatorio());
