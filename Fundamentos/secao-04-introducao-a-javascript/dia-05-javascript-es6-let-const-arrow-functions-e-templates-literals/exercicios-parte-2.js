daa0ebe525c86dcc9cb07d32bc67eb1f2d9b4859
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-01 18:35:55 -0300

    Cria uma funçã que retorna a maior palavra de uma frase.

--- Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-2.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-2.js
@@ -1,4 +1,18 @@
-let status = "ligado";
-let ligarDesligar = () =>
-  status === "ligado" ? "O motor está ligado" : "O motor está desligado";
-console.log(ligarDesligar());
+// let status = "ligado";
+// let ligarDesligar = () =>
+//   status === "ligado" ? "O motor está ligado" : "O motor está desligado";
+// console.log(ligarDesligar());
+
+function biggerWord(phrase) {
+  let phraseArray = phrase.split(" ");
+  let bigger = 0;
+  let word = "";
+  for (index = 0; index < phraseArray.length; index++) {
+    if (phraseArray[index].length >= bigger) {
+      bigger = phraseArray[index].length;
+      word = phraseArray[index];
+    }
+  }
+  console.log(word);
+}
+biggerWord("Antônio foi no banheiro e não sabemos o que aconteceu");
