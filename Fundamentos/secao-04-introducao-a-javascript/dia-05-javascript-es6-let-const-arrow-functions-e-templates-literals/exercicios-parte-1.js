afcc22d6487af0658ebbfe6ec96f476f8628bf45
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-01 15:29:55 -0300

    Corrige o erro de escopo da variável favoriteFood

--- Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
@@ -7,13 +7,17 @@
 // imprimeIdade();
 
 // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
-let pessoa = {
-  nome: "Henri",
-  idade: 20,
-};
-pessoa = {
-  nome: "Luna",
-  idade: 19,
-}; // Altere essa estrutura para corrigir o erro.
-console.log("Nome:", pessoa.nome);
-console.log("Idade:", pessoa.idade);
+// let pessoa = {
+//   nome: "Henri",
+//   idade: 20,
+// };
+// pessoa = {
+//   nome: "Luna",
+//   idade: 19,
+// }; // Altere essa estrutura para corrigir o erro.
+// console.log("Nome:", pessoa.nome);
+// console.log("Idade:", pessoa.idade);
+
+let favoriteFood = "Lasanha";
+favoriteFood = "Hamburguer";
+console.log(favoriteFood);
