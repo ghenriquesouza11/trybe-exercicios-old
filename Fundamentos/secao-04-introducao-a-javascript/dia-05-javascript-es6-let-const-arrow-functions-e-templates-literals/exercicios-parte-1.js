35abae5c9b8b508870b95d3332ab21934117019a
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-01 15:54:27 -0300

    Adequa a sintaxe do cógigo para utilizar template literals.

--- Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicios-parte-1.js
@@ -18,6 +18,17 @@
 // console.log("Nome:", pessoa.nome);
 // console.log("Idade:", pessoa.idade);
 
-let favoriteFood = "Lasanha";
-favoriteFood = "Hamburguer";
-console.log(favoriteFood);
+// let favoriteFood = "Lasanha";
+// favoriteFood = "Hamburguer";
+// console.log(favoriteFood);
+
+const name = "Adriana";
+const lastName = "Soares";
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a, b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de  ${a} + ${b} é: ${soma(a, b)}`);
