29fc687f697b265dc5b4c22671e68e2af1ee47cf
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-07 19:16:25 -0300

    Resolução do exercício 5

--- Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,14 +1,22 @@
 const paragraph = document.querySelectorAll("p");
 const mainContent = document.querySelectorAll(".main-content");
 const centerContent = document.querySelectorAll(".center-content");
+const h1 = document.querySelectorAll("h1");
 
 function changeText(selector, text) {
-  selector[1].innerText = text;
+  selector.innerText = text;
 }
-changeText(paragraph, "Mais culto e mais rico.");
 
 function changeBackgroundColor(selector, color) {
-  selector[0].style.background = color;
+  selector.style.background = color;
 }
-changeBackgroundColor(mainContent, "rgb(76,164,109)");
-changeBackgroundColor(centerContent, "white");
+
+function upperCaseText(selector) {
+  selector.style.textTransform = "uppercase";
+}
+
+changeText(paragraph[1], "Mais culto e mais rico.");
+changeBackgroundColor(mainContent[0], "rgb(76,164,109)");
+changeBackgroundColor(centerContent[0], "white");
+changeText(h1[0], "Desafio - JavaScript");
+upperCaseText(paragraph[0]);
