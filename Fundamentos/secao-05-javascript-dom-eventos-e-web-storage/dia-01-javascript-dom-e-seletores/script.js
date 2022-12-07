5d70f8d0f0ab976220181b26bde81d83076ef84d
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-07 18:54:28 -0300

    Resolução do exercício 2

--- Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,5 +1,12 @@
 const paragraph = document.querySelectorAll("p");
+let maincontent = document.getElementsByClassName("main-content");
+
 function changeText(selector, text) {
   selector[1].innerText = text;
 }
 changeText(paragraph, "Mais culto e mais rico.");
+
+function changeBackgroundColor(selector, color) {
+  selector[0].style.background = color;
+}
+changeBackgroundColor(maincontent, "rgb(76,164,109)");
