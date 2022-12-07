6005ffef4de9b8a1095cf3e926210a82966f8204
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-07 18:58:37 -0300

    Resolução do exercício 3

--- Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,5 +1,6 @@
 const paragraph = document.querySelectorAll("p");
-let maincontent = document.getElementsByClassName("main-content");
+const mainContent = document.querySelectorAll(".main-content");
+const centerContent = document.querySelectorAll(".center-content");
 
 function changeText(selector, text) {
   selector[1].innerText = text;
@@ -9,4 +10,5 @@ changeText(paragraph, "Mais culto e mais rico.");
 function changeBackgroundColor(selector, color) {
   selector[0].style.background = color;
 }
-changeBackgroundColor(maincontent, "rgb(76,164,109)");
+changeBackgroundColor(mainContent, "rgb(76,164,109)");
+changeBackgroundColor(centerContent, "white");
