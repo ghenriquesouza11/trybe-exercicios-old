b2934b3ac7dd5820e2d9201e346b04c0afe215eb
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-13 18:40:54 -0300

    Estabelece o funcionamento correto dos botões para alterar o tamanho da fonte.

--- Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -29,3 +29,19 @@ for (index = 1; index < mainHeaderList[1].children.length; index += 1) {
     }
   });
 }
+for (index = 1; index < mainHeaderList[2].children.length; index += 1) {
+  mainHeaderList[2].children[index].addEventListener("click", function () {
+    // Botões para alterar o tamanho da fonte
+    if (event.target === mainHeaderList[2].children[1].children[0]) {
+      body.style.fontSize = "11px";
+    } else if (event.target === mainHeaderList[2].children[2].children[0]) {
+      body.style.fontSize = "13px";
+    } else if (event.target === mainHeaderList[2].children[3].children[0]) {
+      body.style.fontSize = "16px";
+    } else if (event.target === mainHeaderList[2].children[4].children[0]) {
+      body.style.fontSize = "19px";
+    } else if (event.target === mainHeaderList[2].children[5].children[0]) {
+      body.style.fontSize = "27px";
+    }
+  });
+}
