7d382a5d34d48d115b0b2c00a3450bf593eb1a98
Gustavo Henrique <ghenriquesouza11@gmail.com>
2022-12-13 18:31:46 -0300

    Estabelece o funcionamento correto dos botões para alterar a cor de fundo.

--- Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ Fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,1 +1,19 @@
+const body = document.getElementById("body");
+const mainHeaderList = document.getElementsByClassName("main-header__list");
 
+for (index = 1; index < mainHeaderList[0].children.length; index += 1) {
+  mainHeaderList[0].children[index].addEventListener("click", function () {
+    // Botões para alter a cor de fundo.
+    if (event.target === mainHeaderList[0].children[1].children[0]) {
+      body.style.backgroundColor = "white";
+    } else if (event.target === mainHeaderList[0].children[2].children[0]) {
+      body.style.backgroundColor = "black";
+    } else if (event.target === mainHeaderList[0].children[3].children[0]) {
+      body.style.backgroundColor = "green";
+    } else if (event.target === mainHeaderList[0].children[4].children[0]) {
+      body.style.backgroundColor = "blue";
+    } else if (event.target === mainHeaderList[0].children[5].children[0]) {
+      body.style.backgroundColor = "yellow";
+    }
+  });
+}
