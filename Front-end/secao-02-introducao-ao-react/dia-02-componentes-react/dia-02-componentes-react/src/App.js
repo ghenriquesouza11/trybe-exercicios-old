cee4be4366694856058886c4b77c2139641d295d
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-03-01 01:12:28 -0300

    Renderização de listas utilizando React

--- Front-end/secao-02-introducao-ao-react/dia-02-componentes-react/dia-02-componentes-react/src/App.js
+++ Front-end/secao-02-introducao-ao-react/dia-02-componentes-react/dia-02-componentes-react/src/App.js
@@ -1,13 +1,15 @@
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
-import Image from './Img';
+import UserProfile from './UserProfile';
+import users from './Users';
 
-function App() {
-  return (
-    <Image
-    source ='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
-    alternativeText = 'Cute cat staring'
-    />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      users.map(user => <UserProfile key={user.id} user = {user} />)
+    );
+  }
 }
 
 export default App;
